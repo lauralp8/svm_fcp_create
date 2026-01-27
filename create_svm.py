075e771f7db05477baf9bb0cc21b81009c702542
lauralp8 <lassol@netapp.com>
2026-01-27 09:44:42 +0100

    config.yaml added

--- create_svm.py
+++ create_svm.py
@@ -2,7 +2,14 @@ from netapp_ontap import config, HostConnection, NetAppRestError
 from netapp_ontap.resources import Svm
 import yaml
 
-print()
+
+def config_loader(path="config.yaml"):
+    print("Openning config.yaml for SVM creation parameters...")
+    with open("config.yaml", 'r') as file:
+        config_data = yaml.safe_load(file)
+    print("Config data loaded successfully.")
+    return config_data
+
 
 config.CONNECTION = HostConnection(
     'cluster1.demo.netapp.com',
